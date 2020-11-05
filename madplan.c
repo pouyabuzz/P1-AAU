f959df351670754cd73cb2be10ed0b1555bbddbd
Kevin Thor Hansen <ktha20@student.aau.dk>
2020-11-05 14:47:28 +0100

    Global variables and functions removed

--- madplan.c
+++ madplan.c
@@ -1,137 +1,157 @@
 #include <stdio.h>
-#include <math.h>
 
-    void man_or_woman (void);
-    void bmicalculator (void);
-    void bmrcalculator (void);
-    void purpose (void);
-    void calorieconsumption (void);
-    void foodplan (void);
-    void gain (void);
-    void maintain (void);
-    void lose (void);
 
-    int gender, goal, age, ok;
-    double weight, height, bmi, intake, kcal;
+void run_program ();
+void man_or_woman (int *gender, int *age, double *weight, double *height); 
+void bmi_calculator (double weight, double height);
+void bmr_calculator (int gender, double *bmr, double weight, double height, int age);
+void consumption_func (double bmr, double *consumption, double activity_level);
+void activity (double activity_level);
+void mealplan (double consumption);
 
-    int main (void) {
-        man_or_woman();
-        bmicalculator();
-        bmrcalculator();
-        purpose();
-        foodplan();
-    }
-    void man_or_woman () {
-        printf("Insert 1 if you are a woman, and insert 2 if you are a man\n");
-        scanf("%d", &gender);
-        if (gender = 2) {
-            printf("What is your weight sir?\n");    
-            scanf("%lf", &weight);
-            printf("What is your height sir?\n");
-            scanf("%lf", &height);
-            printf("What is your age sir?\n");
-            scanf("%lf", &age);
-        }
-        else if (gender = 1){
-            printf("What is your weight Ma'am?\n");    
-            scanf("%lf", &weight);
-            printf("What is your height Ma'am?\n");
-            scanf("%lf", &height);
-            printf("What is your age Ma'am?\n");
-            scanf("%lf", &age);
-        }
-    }
-    void bmicalculator () {
-        bmi = weight /((height/100) * (height/100)) ;
-        printf("Your BMI is %f\n", bmi);
+int main (){
+
+    run_program ();
+      
+    return 0;
+}
+
+void run_program (){
+    
+    int gender, age;
+    double bmr, weight, height, activity_level, consumption;
+    
+    man_or_woman (&gender, &age, &weight, &height);
+    bmi_calculator (weight, height);
+    bmr_calculator (gender, &bmr, weight, height, age);
+    /*activity (&activity_level);*/
+    consumption_func (bmr, &consumption, activity_level);
+    mealplan (consumption);
+
+}
+    void man_or_woman (int *gender, int *age, double *weight, double *height) {
         
+        printf("Insert 1 if you are a woman, and insert 2 if you are a man\n");
+        scanf("%d", gender);
+        if (*gender == 2) {
+            printf("What is your weight sir?\n");    
+            scanf("%lf", weight);
+            printf("What is your height sir?\n");
+            scanf("%lf", height);
+            printf("What is your age sir?\n");
+            scanf("%d", age);
+        }
+        else if (*gender == 1){
+            printf("What is your weight Ma'am?\n");    
+            scanf("%lf", weight);
+            printf("What is your height Ma'am?\n");
+            scanf("%lf", height);
+            printf("What is your age Ma'am?\n");
+            scanf("%d", age);
+        }
+    }
+    void bmi_calculator (double weight, double height) {
+        double bmi = weight / ((height / 100) * (height / 100)) ;
+        printf("Your BMI is %lf \n", bmi);
+    
+    }
+    void bmr_calculator (int gender, double *bmr, double weight, double height, int age) {
+        if (gender == 1){
+        *bmr = (10 * weight) + (6.25 * height) - (5 * age) - 161;
+        printf("Your BMR is estimated to be %f\n", *bmr);
+        }
+        else if (gender == 2){
+        *bmr = (10 * weight) + (6.25 * height) - (5 * age) + 5;
+        printf("Your BMR is estimated to be %f\n", *bmr);
+        }
+        
+    }
 
-    }
-    void bmrcalculator () {
-        if (gender = 1){
-        kcal = (10 * weight) + (6.25 * height) - (5*age) - 161;
-        printf("Your BMR is estimated to be %f\n", kcal);
-        }
-        else if (gender =2){
-        kcal = (10 * weight) + (6.25 * height) - (5*age) + 5;
-        printf("Your BMR is estimated to be %f\n", kcal);
-        }
-    }
-    void purpose () {
-        printf("Insert 1 if you wish to lose weight.\n");
-        printf("Insert 2 if you wish to maintain your weight.\n");
-        printf("Insert 3 if you wish to gain more weight.\n");
-        scanf("%d", &goal);
-        if (goal == 1) {
-            printf("Based on your BMI, which is %f, and the fact that you wish to lose weight.\n", bmi);
-            lose();
-        }
+    //Activity Level Function
 
-        else if (goal == 2) {
-            printf("Based on your BMI, which is %f, and the fact that you wish to maintain weight.\n", bmi);
-            maintain();
-        }
-        else if (goal == 3) {
-            printf("Based on your BMI, which is %f, and the fact that you wish to gain weight.\n", bmi);
-            gain();
-        }
-    }
-    void lose (){
-        if (bmi<=20) {
-            intake = 15000 - kcal;
-        }
-        else if ((bmi>20) && (bmi<=25)) {
-            intake = 16000 - kcal;
-        }
-        else if (bmi>=25) {
-            intake = 17000 - kcal;
-            }
-        printf("Your daily calorie intake should be %f\n", intake);
-    }
-    void maintain (){
-        if (bmi<=20) {
-            intake = 17000 - kcal;
-        }
-        else if ((bmi>20) && (bmi<=25)) {
-            intake = 18000 - kcal;
-        }
-        else if (bmi>=25) {
-            intake = 19000 - kcal;
-            }
-        printf("Your daily calorie intake should be %f\n", intake);
-    }
-    void gain (){
-        if (bmi<=20) {
-            intake = 20000 - kcal;
-        }
-        else if ((bmi>20) && (bmi<=25)) {
-            intake = 21000 - kcal;
-        }
-        else if (bmi>=25) {
-            intake = 22000 - kcal;
-            }
-        printf("Your daily calorie intake should be %f\n", intake);
-    }
-    void foodplan () {
-        printf("Would you like to see your mealplan?\n");
-        printf("Then insert 1\n");
-        scanf("%d", &ok);
-        if (ok = 1) {
-            if (intake>=20000) {
-                printf("Breakfast: Havregryn\n");
-                printf("Lunch: Havregryn\n");
-                printf("Dinner: Havregryn\n");
-            }
-            else if (intake>=19999 && intake<=18000) {
-                printf("Breakfast: Rugbrød\n");
-                printf("Lunch: Rugbrød\n");
-                printf("Dinner: Rugbrød\n");
-            }
-            else if (intake<= 17999) {
-                printf("Breakfast: Toast\n");
-                printf("Lunch: Toast\n");
-                printf("Dinner: Toast\n");
-            }
-        }
+                        /*void purpose (int *goal, double *bmi, double *consumption, double *kcal) {
+                            printf("Insert 1 if you wish to lose weight.\n");
+                            printf("Insert 2 if you wish to maintain your weight.\n");
+                            printf("Insert 3 if you wish to gain more weight.\n");
+                            scanf("%d", goal);
+                            if (*goal == 1) {
+                                printf("Based on your BMI, which is %f, and the fact that you wish to lose weight.\n", *bmi);
+                                consumption (goal);
+                            }
 
+                            else if (*goal == 2) {
+                                printf("Based on your BMI, which is %f, and the fact that you wish to maintain weight.\n", *bmi);
+                                consumption (goal);
+                            }
+                            else if (*goal == 3) {
+                                printf("Based on your BMI, which is %f, and the fact that you wish to gain weight.\n", *bmi);
+                                consumption (goal);
+                            }
+                        }*/
+                        
+                        /*void lose (double *bmr, double *activity){
+                            double consumption = (bmr * activity) - 500;
+                            printf("Your daily calorie intake should be %f\n", consumption);
+                        }
+                        void maintain (double *bmr, double *activity){
+                            double consumption = (bmr*activity);
+                            printf("Your daily calorie intake should be %f\n", consumption);
+                        }
+                        void gain (double *bmr, double *activity){
+                            double consumption = (bmr*activity)+500;
+                            printf("Your daily calorie intake should be %f\n", consumption);
+                        }*/
+void consumption_func(double bmr, double *consumption, double activity_level){
+
+    int goal;
+    
+    printf("Insert 1 if you wish to lose weight.\n");
+    printf("Insert 2 if you wish to maintain your weight.\n");
+    printf("Insert 3 if you wish to gain more weight.\n");
+    scanf("%d", &goal);
+
+    switch (goal){
+
+    case 1:
+        *consumption = (bmr * activity_level) - 500;
+        printf("Your daily calorie intake should be %f\n", *consumption);
+        
+        break;
+
+    case 2:
+        *consumption = (bmr * activity_level);
+        printf("Your daily calorie intake should be %f\n", *consumption);
+
+        break;
+
+    case 3: 
+        *consumption = (bmr * activity_level) + 500;
+        printf("Your daily calorie intake should be %f\n", *consumption);
+
+        break;
+    
     }
+
+}
+
+
+void mealplan (double consumption) {
+    
+    if (consumption >= 20000) {
+        printf("Breakfast: Havregryn\n");
+        printf("Lunch: Havregryn\n");
+        printf("Dinner: Havregryn\n");
+    }
+    else if (consumption >= 19999 && consumption <= 18000) {
+        printf("Breakfast: Rugbrød\n");
+        printf("Lunch: Rugbrød\n");
+        printf("Dinner: Rugbrød\n");
+    }    
+    else if (consumption <= 17999) {
+        printf("Breakfast: Toast\n");
+        printf("Lunch: Toast\n");
+        printf("Dinner: Toast\n");
+    }
+}
+
+    
