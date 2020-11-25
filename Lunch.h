2569dc8a79d5f689f1ba7723bc12edbd89d294c7
DESKTOP-L5B6ODB\charl <cthaar20@student.aau.dk>
2020-11-25 12:24:11 +0100

    Adding Lunch.h

---
+++ Lunch.h
@@ -0,0 +1,22 @@
+
+void lunch_meal(int consumption);
+
+void cottage_cheese_and_raspberries();
+
+void cucumber_avocado_salad();
+
+void curry_tuna_salad();
+
+void peanut_butter_yogurt();
+
+void turkey_sandwiches();
+
+void ham_and_salami_rollups();
+
+void almond_mango_protein_shake();
+
+void apricot_jam_and_almond_butter_sandwich();
+
+void roast_beef_and_cheddar_sandwich();
+
+void cucumber_avocado_salad();
