ae2dcffdcf54158c16183adc076ebb4d6bd41117
DESKTOP-L5B6ODB\charl <cthaar20@student.aau.dk>
2020-12-08 16:48:23 +0100

    Updated lunch files

--- Lunch.c
+++ Lunch.c
@@ -1,11 +1,12 @@
 #include "Lunch.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
 #define MAX_char 2000
-#define MAX_ING  20
+#define MAX_ING 20
 #define NR_RECIPES 11
 
 // recipes is from eatthismuch.com
@@ -36,143 +37,145 @@ struct recipes {
     lunch_meal(a);
 }*/
 
-int lunch_meal(int consumption){
+int lunch_meal(int consumption) {
     srand(time(NULL));
     int j, i = (rand() % NR_RECIPES);
     struct recipes *recipes_array = read_file_lunch();
 
-    if (consumption <= 400){
-       do{
-            if(recipes_array[i].kcal >= 200 && recipes_array[i].kcal <= 400){
-                
-                printf("Kcal: %d\n%s\n%s\n", recipes_array[i].kcal, recipes_array[i].name, recipes_array[i].time);
-                for(j = 0; j < recipes_array[i].nr_ing; j++){
+    if (consumption <= 400) {
+        do {
+            if (recipes_array[i].kcal >= 200 && recipes_array[i].kcal <= 400) {
+                printf("Kcal: %d\n%s\n%s\n", recipes_array[i].kcal,
+                       recipes_array[i].name, recipes_array[i].time);
+                for (j = 0; j < recipes_array[i].nr_ing; j++) {
                     printf("%s", recipes_array[i].ingrediens[j]);
                 }
-                for(j = 0; j < recipes_array[i].nr_steps; j++){
+                for (j = 0; j < recipes_array[i].nr_steps; j++) {
                     printf("%s", recipes_array[i].steps[j]);
                 }
                 printf("\n");
                 consumption = recipes_array[i].kcal;
-            }else{
+            } else {
                 i = (rand() % NR_RECIPES);
             }
-       }while(!(consumption == recipes_array[i].kcal));
+        } while (!(consumption == recipes_array[i].kcal));
 
-    }else if(consumption <= 600){
-        do{
-            if(recipes_array[i].kcal > 400 && recipes_array[i].kcal <= 600){
-                
-                printf("Kcal: %d\n%s\n%s\n", recipes_array[i].kcal, recipes_array[i].name, recipes_array[i].time);
-                for(j = 0; j < recipes_array[i].nr_ing; j++){
+    } else if (consumption <= 600) {
+        do {
+            if (recipes_array[i].kcal > 400 && recipes_array[i].kcal <= 600) {
+                printf("Kcal: %d\n%s\n%s\n", recipes_array[i].kcal,
+                       recipes_array[i].name, recipes_array[i].time);
+                for (j = 0; j < recipes_array[i].nr_ing; j++) {
                     printf("%s", recipes_array[i].ingrediens[j]);
                 }
-                for(j = 0; j < recipes_array[i].nr_steps; j++){
+                for (j = 0; j < recipes_array[i].nr_steps; j++) {
                     printf("%s", recipes_array[i].steps[j]);
                 }
                 printf("\n");
                 consumption = recipes_array[i].kcal;
-               }else{
-                   i = (rand() % NR_RECIPES);
-               }
-       }while(!(consumption == recipes_array[i].kcal));
+            } else {
+                i = (rand() % NR_RECIPES);
+            }
+        } while (!(consumption == recipes_array[i].kcal));
 
-    }else if(consumption <= 800){
-        do{
-            if(recipes_array[i].kcal > 600 && recipes_array[i].kcal <= 800){
-                printf("Kcal: %d\n%s\n%s\n", recipes_array[i].kcal, recipes_array[i].name, recipes_array[i].time);
-                for(j = 0; j < recipes_array[i].nr_ing; j++){
+    } else if (consumption <= 800) {
+        do {
+            if (recipes_array[i].kcal > 600 && recipes_array[i].kcal <= 800) {
+                printf("Kcal: %d\n%s\n%s\n", recipes_array[i].kcal,
+                       recipes_array[i].name, recipes_array[i].time);
+                for (j = 0; j < recipes_array[i].nr_ing; j++) {
                     printf("%s", recipes_array[i].ingrediens[j]);
                 }
-                for(j = 0; j < recipes_array[i].nr_steps; j++){
+                for (j = 0; j < recipes_array[i].nr_steps; j++) {
                     printf("%s", recipes_array[i].steps[j]);
                 }
                 printf("\n");
                 consumption = recipes_array[i].kcal;
-               }else{
-                   i = (rand() % NR_RECIPES);
-               }
-       }while(!(consumption == recipes_array[i].kcal));
+            } else {
+                i = (rand() % NR_RECIPES);
+            }
+        } while (!(consumption == recipes_array[i].kcal));
 
-    }else if(consumption <= 1000){
-        do{
-            if(recipes_array[i].kcal > 800 && recipes_array[i].kcal <= 1000){
-                printf("Kcal: %d\n%s\n%s\n", recipes_array[i].kcal, recipes_array[i].name, recipes_array[i].time);
-                for(j = 0; j < recipes_array[i].nr_ing; j++){
+    } else if (consumption <= 1000) {
+        do {
+            if (recipes_array[i].kcal > 800 && recipes_array[i].kcal <= 1000) {
+                printf("Kcal: %d\n%s\n%s\n", recipes_array[i].kcal,
+                       recipes_array[i].name, recipes_array[i].time);
+                for (j = 0; j < recipes_array[i].nr_ing; j++) {
                     printf("%s", recipes_array[i].ingrediens[j]);
                 }
-                for(j = 0; j < recipes_array[i].nr_steps; j++){
+                for (j = 0; j < recipes_array[i].nr_steps; j++) {
                     printf("%s", recipes_array[i].steps[j]);
                 }
                 printf("\n");
                 consumption = recipes_array[i].kcal;
-                }else{
-                   i = (rand() % NR_RECIPES);
-                }
-       }while(!(consumption == recipes_array[i].kcal));
+            } else {
+                i = (rand() % NR_RECIPES);
+            }
+        } while (!(consumption == recipes_array[i].kcal));
 
-    }else {
-         do{
-             printf("%d", i);
-            if(recipes_array[i].kcal > 1000){
-                printf("Kcal: %d\n%s\n%s\n", recipes_array[i].kcal, recipes_array[i].name, recipes_array[i].time);
-                for(j = 0; j < recipes_array[i].nr_ing; j++){
+    } else {
+        do {
+            if (recipes_array[i].kcal > 1000) {
+                printf("Kcal: %d\n%s\n%s\n", recipes_array[i].kcal,
+                       recipes_array[i].name, recipes_array[i].time);
+                for (j = 0; j < recipes_array[i].nr_ing; j++) {
                     printf("%s", recipes_array[i].ingrediens[j]);
                 }
-                for(j = 0; j < recipes_array[i].nr_steps; j++){
+                for (j = 0; j < recipes_array[i].nr_steps; j++) {
                     printf("%s", recipes_array[i].steps[j]);
                 }
                 printf("\n");
                 consumption = recipes_array[i].kcal;
-                }else{
-                    i = (rand() % NR_RECIPES);
-                }
-       }while(!(consumption == recipes_array[i].kcal));
-    } 
+            } else {
+                i = (rand() % NR_RECIPES);
+            }
+        } while (!(consumption == recipes_array[i].kcal));
+    }
     return consumption;
     free(recipes_array);
 }
 
-struct recipes *read_file_lunch(){
+struct recipes *read_file_lunch() {
     struct recipes *recipes_array = allocate_struct_lunch();
     int i, j = 0;
     char line[2000];
 
     FILE *f = fopen("lunch_recipes.txt", "r");
-    if(f == NULL){
+    if (f == NULL) {
         printf("File cannot open");
         exit(EXIT_FAILURE);
     }
- 
-    for(i = 0; i < NR_RECIPES; ++i){
+
+    for (i = 0; i < NR_RECIPES; ++i) {
         fscanf(f, " @%d\n", &recipes_array[i].kcal);
         fscanf(f, " %[-.&A-Za-z ]\n", recipes_array[i].name);
-        fscanf(f, " %[.,0-9A-Za-z ]\n", recipes_array[i].time);
+        fscanf(f, " %[.,0-9A-Za-z& ]\n", recipes_array[i].time);
         fscanf(f, " %d\n", &recipes_array[i].nr_ing);
-        do{
+        do {
             fgets(line, sizeof(line), f);
             strcpy(recipes_array[i].ingrediens[j], line);
             j++;
-            
-        } while(j < recipes_array[i].nr_ing);
+
+        } while (j < recipes_array[i].nr_ing);
         j = 0;
         fscanf(f, " %d\n", &recipes_array[i].nr_steps);
-        do{
+        do {
             fgets(line, sizeof(line), f);
             strcpy(recipes_array[i].steps[j], line);
             j++;
-            
-        } while(j < recipes_array[i].nr_steps);
+
+        } while (j < recipes_array[i].nr_steps);
         j = 0;
     }
 
-  fclose(f);
-  return recipes_array;
+    fclose(f);
+    return recipes_array;
 }
 
-struct recipes *allocate_struct_lunch(){
+struct recipes *allocate_struct_lunch() {
     struct recipes *recipes_array = malloc(20 * sizeof(struct recipes));
-    if(recipes_array == NULL){
+    if (recipes_array == NULL) {
         printf("kan ikke allokere nok hukommelse. Farvel\n");
         exit(EXIT_FAILURE);
     }
